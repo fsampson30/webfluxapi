43f0dcf3cc4762de1548ab9dddca6801d99dc52d
fsampson30 <fsampson30@gmail.com>
2022-12-12 09:56:12 -0300

    Created PlaylistRepository.java.

---
+++ src/main/java/repository/PlaylistRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import document.Playlist;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface PlaylistRepository extends ReactiveMongoRepository<Playlist, String> {
+}
