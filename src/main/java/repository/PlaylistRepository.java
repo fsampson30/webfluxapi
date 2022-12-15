17ef5ff1337ec1aed924455ed754e91846fe118b
fsampson30 <fsampson30@gmail.com>
2022-12-15 07:32:42 -0300

    Created service classes.

--- src/main/java/repository/PlaylistRepository.java
+++ src/main/java/repository/PlaylistRepository.java
@@ -2,7 +2,6 @@ package repository;
 
 import document.Playlist;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
 
 public interface PlaylistRepository extends ReactiveMongoRepository<Playlist, String> {
 }
