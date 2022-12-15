13a33e45807f0002af3912987491a35f5a671543
fsampson30 <fsampson30@gmail.com>
2022-12-15 09:04:09 -0300

    Added new method to response every 10s.

--- src/main/java/com/sampson/webfluxapi/controller/PlaylistController.java
+++ src/main/java/com/sampson/webfluxapi/controller/PlaylistController.java
@@ -2,10 +2,15 @@ package com.sampson.webfluxapi.controller;
 
 import com.sampson.webfluxapi.document.Playlist;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import com.sampson.webfluxapi.services.PlaylistService;
+import reactor.util.function.Tuple2;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 //@RestController
 public class PlaylistController {
@@ -26,5 +31,16 @@ public class PlaylistController {
     @PostMapping(value = "/playlist")
     public Mono<Playlist> savePlaylist(@RequestBody Playlist playlist){
         return playlistService.Save(playlist);
+    }
+
+    @GetMapping(value="/playlist/webflux", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Tuple2<Long, Playlist>> getPlaylistByWebflux(){
+
+        System.out.println("---Start get Playlists by WEBFLUX--- " + LocalDateTime.now());
+        Flux<Long> interval = Flux.interval(Duration.ofSeconds(10));
+        Flux<Playlist> playlistFlux = playlistService.findAll();
+
+        return Flux.zip(interval, playlistFlux);
+
     }
 }
