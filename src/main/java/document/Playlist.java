3418f0c06d7bb8433612be2cf1ac85df1f171487
fsampson30 <fsampson30@gmail.com>
2022-12-15 07:21:07 -0300

    Removed unused class DummyData.

--- src/main/java/document/Playlist.java
+++ src/main/java/document/Playlist.java
@@ -6,6 +6,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document
 public class Playlist {
 
+
     @Id
     private String id;
     private String nome;
--- src/main/java/repository/PlaylistRepository.java
+++ src/main/java/repository/PlaylistRepository.java
@@ -2,6 +2,7 @@ package repository;
 
 import document.Playlist;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 
 public interface PlaylistRepository extends ReactiveMongoRepository<Playlist, String> {
 }
